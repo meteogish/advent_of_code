d4d5a3d4312fe44916399984b892c361dd00fa3d
Yevhenii Kyshko <meteogish@gmail.com>
2020-11-03 22:56:46 +0100

    More refactor. Very slow

--- 2017/day6.fsx
+++ 2017/day6.fsx
@@ -1,6 +1,6 @@
-let input = 
+let input =
     (System.IO.File.ReadAllLines("input_day6.txt")
-    |> Seq.head).Split('\t')
+     |> Seq.head).Split('\t')
     |> Seq.map int
     |> Array.ofSeq
 
@@ -8,54 +8,58 @@ printfn "%A" input
 
 let len = input.Length
 
-let findMaxIndex banks =
-    banks 
+let equals b1 b2 =
+    (b1 |> Seq.exists2 (fun i1 i2 -> i1 <> i2) b2)
+    |> not
+
+let printBanks = 
+    Array.ofSeq >> printfn "%A"
+
+let findMax banks =
+    banks
     |> Seq.mapi (fun i b -> (i, b))
     |> Seq.maxBy snd
-    |> fst
 
 let increaseBanksIn indexToIncrease banks =
-    banks 
+    banks
     |> Seq.mapi (fun i b -> if i = indexToIncrease then b + 1 else b)
 
 let zeroBanksIn indexToZero banks =
-    banks 
+    banks
     |> Seq.mapi (fun i b -> if i = indexToZero then 0 else b)
 
-let redistribute len currentBanks maxIndex =
-   let banksAmount = currentBanks |> Seq.item maxIndex
-   let rest = banksAmount % (len - 1) 
-   let by = (banksAmount - rest) / (len - 1)
-   printfn "Redistribute: max: %d; len: %d; rest: %d; by: %d" banksAmount len rest by 
+let redistribute len currentBanks =
+    let (maxIndex, max) = currentBanks |> findMax 
+    //printfn "Redistribute: max: %d; atIndex: %d" max maxIndex 
 
-   currentBanks
-   |> Seq.mapi (fun i b -> if i = maxIndex then rest else b + by)  
+    [ 1 .. max ]
+    |> Seq.fold (fun acc i -> acc |> increaseBanksIn ((i + maxIndex) % len)) (currentBanks |> zeroBanksIn maxIndex)
 
 let proc banks len =
+    let rec procAcc len stack bs =
+        let redbtd = redistribute len bs 
+        //printBanks redbtd
 
-    let rec procAcc len stack bs  =
-        let redbtd = redistribute len bs ( bs |> findMaxIndex )
-        if stack |> List.exists (fun b -> (redbtd |> Seq.compareWith Operators.compare b) = 0) then
-            (redbtd :: stack)
+        if ((stack |> List.length) % 10) = 0 then
+            printfn "Stack size: %d " stack.Length
+        
+        if stack |> List.exists (equals redbtd) then
+            stack
         else
             procAcc len (redbtd :: stack) redbtd
-    
-    procAcc len List.empty banks
 
-   
+    procAcc len [banks] banks
 
-let testInput = seq { 0; 2; 7; 0 }
-let firstCycleMaxIndex = testInput |> findMaxIndex
-//let firstCycle = redistribute firstCycleMaxIndex testInput (testInput |> Seq.length)
-//printfn "%A" (firstCycle |> Array.ofSeq)
 
-//let processed = proc testInput (testInput |> Seq.length) 
-//processed |> Seq.rev |> Seq.iter (fun line -> printfn "%A" (line |> Array.ofSeq))
-//printfn "%A" processed.Length
+let testInput = seq { 0; 2; 7; 0; }
 
+let processed = proc testInput (testInput |> Seq.length)
+processed |> Seq.rev |> Seq.iter printBanks   
+printfn "%A" processed.Length
+
+//[ 1 .. 7 ]
+//|> Seq.iter (printfn "%d")
 
 let answerPartOne = proc input len
-answerPartOne |> Seq.rev |> Seq.iter (fun line -> printfn "%A" (line |> Array.ofSeq))
+
 printfn "AnswerPartOne: %d" answerPartOne.Length
-
-
