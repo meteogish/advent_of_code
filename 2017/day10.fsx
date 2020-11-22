64f82d50d0a8614bfc604db1e2bfa87174b36a13
Yevhenii Kyshko <meteogish@gmail.com>
2020-11-22 23:09:35 +0100

    ✨ Day10 Part Two working

--- 2017/day10.fsx
+++ 2017/day10.fsx
@@ -2,7 +2,8 @@
 
 open System.Collections.Generic
 
-let day10 = System.IO.File.ReadAllText("inputs/input_day10.txt").Split(',') |> Array.map int
+let day10PartOne = System.IO.File.ReadAllText("inputs/input_day10.txt").Split(',') |> Array.map int
+let day10PartTwo = [| 17; 31; 73; 47; 23 |] |> Array.append (System.IO.File.ReadAllBytes("inputs/input_day10.txt") |> Array.map int)
 
 type State = {
     Elements: int array;
@@ -10,26 +11,27 @@ type State = {
     SkipSize: int;
 }
 
-let initialState = {
+let initialState () = {
     Elements = { 0 .. 255 } |> Array.ofSeq;
     CurrentPosition = 0;
     SkipSize = 0; 
 }
 
-let rotateBetween state length =
+let knotBy state length =
     let len = state.Elements.Length
 
     let mutable left = state.CurrentPosition
-    let mutable right = (state.CurrentPosition + length) % len - 1
+    let mutable ml = length
 
-    printfn "left = %d; right=%d; state = %A; length = %d;" left right state length
-    while left <> right do 
-        printfn "left: %d; right: %d" left right
+    //printfn "left = %d; state = %A; length = %d;" left state length
+    while ml > 1 do 
+        let right = (left + ml - 1) % len  
+        //printfn "left: %d; right: %d" left right
         let t = state.Elements.[left]
         state.Elements.[left] <- state.Elements.[right]
         state.Elements.[right] <- t
         left <- (left + 1) % len
-        right <- (len + right - 1) % len
+        ml <- ml - 2 
 
     {
         Elements = state.Elements;
@@ -37,22 +39,29 @@ let rotateBetween state length =
         SkipSize = state.SkipSize + 1
     } 
 
-let f input state =
-    let folder = rotateBetween 
-    
-    input |> Seq.fold folder state 
+let elements state = state.Elements
 
-let testState = {
-    Elements = { 0 .. 4 } |> Array.ofSeq;
-    SkipSize = 0;
-    CurrentPosition = 0;
-}
-let lengths = [ 3; 4; 1; 5 ]
-f lengths testState
-|> printfn "%A"
+let knot input repeat state =
+    if repeat = 1 then Seq.singleton 1 else { 1 .. repeat } 
+    |> Seq.fold 
+        (fun acc _ -> 
+            input |> Seq.fold knotBy acc) state
 
-//initialState |> f day10 |> printfn "%A"
+let answerPartOne = 
+    initialState () 
+    |> knot day10PartOne 1 
+    |> elements |> Seq.take 2 
+    |> Seq.reduce (*)
 
-//let testElements = { 0 .. 4 } |> Array.ofSeq
-//printfn "%A" <| rotateBetween testElements 0 2
-//printfn "%A" <| rotateBetween testElements 3 1
+answerPartOne |> printfn "AnswerPartOne: %d"
+
+let sparseHash = initialState () |> knot day10PartTwo 64 |> elements
+
+let denseHash = 
+    sparseHash 
+    |> Array.chunkBySize 16 
+    |> Array.map (fun block -> block |> Seq.reduce (^^^) |> sprintf "%02x")
+    |> String.concat ""
+
+denseHash |> printfn "AnswerPartTwo: %s"
+
