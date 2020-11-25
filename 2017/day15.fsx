f20ca103a0c3f89485d0245da3b8539ee22d50e9
Yevhenii Kyshko <meteogish@gmail.com>
2020-11-25 20:05:28 +0100

    Refactor day15

--- 2017/day15.fsx
+++ 2017/day15.fsx
@@ -1,6 +1,7 @@
+let inputA = 703u
+let inputB = 516u
 
-let inputA = 703
-let inputB = 516
+let getRightmost16Bits x = (x) &&& (uint32 0x0000FFFF)
 
 let rec enumerator pass factor init =
     let f factor (i: uint) =
@@ -13,18 +14,17 @@ let rec enumerator pass factor init =
         yield! enumerator pass factor cur
     }
 
-let getRightmost16Bits x = (x) &&& (uint32 0x0000FFFF)
+let getGen factor input pass = enumerator pass factor input
+let noCheck _ = true
+let genACheck i = i % 4u = 0u
+let genBCheck i = i % 8u = 0u
 
-let genAPartOne =  enumerator (fun _ -> true) 16807UL (uint32 inputA)
-let genBPartOne =  enumerator (fun _ -> true) 48271UL (uint32 inputB)
-
-let genAPartTwo =  enumerator (fun i  -> i % 4u = 0u) 16807UL (uint32 inputA)
-let genBPartTwo =  enumerator (fun i ->  i % 8u = 0u) 48271UL (uint32 inputB)
+let genA =  getGen 16807UL inputA 
+let genB =  getGen 48271UL inputB
 
 let compare left right =
     let leftBits = left |> getRightmost16Bits 
     let rightBits = right |> getRightmost16Bits
-    
     leftBits = rightBits
 
 let judge count genA genB = 
@@ -32,6 +32,5 @@ let judge count genA genB =
     |> Seq.take count 
     |> Seq.fold (fun count (left, right) -> if compare left right then count + 1 else count) 0 
 
-
-judge 40_000_000 genAPartOne genBPartOne |> printfn "AnswerPartOne: %d"
-judge 5_000_000 genAPartTwo genBPartTwo |> printfn "AnswerPartTwo: %d"
+judge 40_000_000 (genA noCheck) (genB noCheck) |> printfn "AnswerPartOne: %d"
+judge 5_000_000 (genA genACheck) (genB genBCheck) |> printfn "AnswerPartTwo: %d"
