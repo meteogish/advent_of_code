16314038a30504d3bcbf819927813ecea9038181
Yevhenii Kyshko <meteogish@gmail.com>
2020-12-01 18:45:52 +0100

    Day17 part two

--- 2017/day17.fsx
+++ 2017/day17.fsx
@@ -1,13 +1,14 @@
 open System.Collections.Generic
 
 let steps = 301
+let count = 2017
 
-let list = List<int> (2018)
+let list = List<int> (count)
 list.Add(0)
 
 
 let rec repeat1 i currentPos (list : List<int>) =
-    if i > 2017 then 
+    if i > count then 
         currentPos
     else 
         let newCurrentPos = (currentPos + steps) % list.Count + 1
@@ -15,8 +16,23 @@ let rec repeat1 i currentPos (list : List<int>) =
         repeat1 (i + 1) newCurrentPos list
 
 let lastPos = repeat1 1 0 list
+list |> Seq.item (lastPos + 1) |> printfn "AnswerPartOne: %d"
 
-list |> Seq.skip (lastPos - 1) |> Seq.take 3 |> printfn "%A"
+
+let fiftyMillion = 50_000_000
+
+let mutable itemAfterZero = 0
+
+let rec repeat2 i currentPos =
+    if i > fiftyMillion then 
+        currentPos
+    else
+        let newCurrentPos = (currentPos + steps) % (i) + 1
+        itemAfterZero <- if newCurrentPos = 1 then i else itemAfterZero
+        repeat2 (i + 1) newCurrentPos
+
+repeat2 1 0 |> printfn "AnswerPartTwo: %d, CurrentPos: %d" itemAfterZero
+         
 
 
 
