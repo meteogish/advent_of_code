e20b0a5059edbb0ac36c6df15811c9f8c5c8ce0d
Yevhenii Kyshko <meteogish@gmail.com>
2022-01-15 23:52:17 +0100

    day3 part2 in paralel

--- 2020/day3.fsx
+++ 2020/day3.fsx
@@ -22,10 +22,10 @@ let partOne = loop start (3, 1) 0
 
 printfn "Part one: %d" partOne
 
-#time    
 let partTwo = 
     [ (1,1); (3,1); (5,1); (7,1); (1, 2)] 
-    |> Seq.map (fun step -> loop start step 0 |> bigint)  
+    |> Seq.map (fun step -> async { return loop start step 0 |> bigint })  
+    |> Async.Parallel
+    |> Async.RunSynchronously
     |> Seq.reduce (*)
-
-printfn "Part two: %A" partTwo
+printfn "Part two: %A" partTwo 
