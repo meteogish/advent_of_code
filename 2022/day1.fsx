08cdec718401bb938dab577d99ab1eaaff36bbcb
Yevhenii Kyshko <meteogish@gmail.com>
2022-12-04 20:20:57 +0100

    day1 formatted

--- 2022/day1.fsx
+++ 2022/day1.fsx
@@ -1,21 +1,23 @@
 open System
 
-let readFileLines path = seq {
-    use file = System.IO.File.OpenRead(path)
-    use reader = new IO.StreamReader(file) 
-    
-    while not reader.EndOfStream 
-        do yield reader.ReadLine()
-}
+let readFileLines path =
+    seq {
+        use file = System.IO.File.OpenRead(path)
+        use reader = new IO.StreamReader(file)
+
+        while not reader.EndOfStream do
+            yield reader.ReadLine()
+    }
 
 let processLine (computed, inHand) line =
     if line |> String.IsNullOrEmpty then
         let sumInHand = inHand |> Seq.sum
-        (computed @ [sumInHand], [])
+        (computed @ [ sumInHand ], [])
     else
-        (computed, inHand @ [line |> int])
-    
-let computedResults = "input_day1" |> readFileLines |> Seq.fold processLine ([], []) |> fst
+        (computed, inHand @ [ line |> int ])
+
+let computedResults =
+    "input_day1" |> readFileLines |> Seq.fold processLine ([], []) |> fst
 
 let topThree = computedResults |> List.sortDescending |> List.take 3
 
