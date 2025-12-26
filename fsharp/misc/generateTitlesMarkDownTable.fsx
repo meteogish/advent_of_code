6057bf8b48a9cf448b23aba4a41237e89afc7cfd
YEKY <YEKY@example.com>
2025-12-26 22:03:00 +0100

    refactor generator to adjust readme

--- fsharp/misc/generateTitlesMarkDownTable.fsx
+++ fsharp/misc/generateTitlesMarkDownTable.fsx
@@ -9,23 +9,23 @@ let getTitle (client: HttpClient) (rx:Regex) year day =
         let! text = client.GetStringAsync (link) |> Async.AwaitTask
         let m = rx.Match (text)
         let res =  m.Groups.[1].Value
-        return (day, res)
+        return (link, day, res)
     }
 
-let toMd year (day, name) =
-    sprintf "| [%s](./%d/day%d.fsx) |" name year day
+let toMd year (link, day, name) =
+    $"| [%s{name}](%s{link}) | [F#](./fsharp/%d{year}/day%d{day}.fsx) |"
 
 let getTitlesMarkDown year =
     use client = new HttpClient ()
     let rx = Regex("<h2>--- (Day \w+: .*) ---<\/h2>", RegexOptions.Compiled)
 
-    { 1 .. 25 } 
+    seq { 1 .. 25 } 
     |> Seq.map (getTitle client rx year)
     |> Async.Parallel 
     |> Async.RunSynchronously 
     |> Seq.map (toMd year)
     
-getTitlesMarkDown 2019
-|> Seq.append [ "| |" ; "| - |" ] 
+getTitlesMarkDown 2017
+|> Seq.append [ "| | |" ; "| - | - |" ] 
 |> String.concat "\n" 
 |> printfn "%s"
