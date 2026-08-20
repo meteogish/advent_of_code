1b6b55d3f9f5c5eb5ee7030be4f97f9558042347
Yevhenii Kyshko <meteogish@gmail.com>
2026-08-20 16:33:59 +0200

    refactor and unification

--- go-solutions/2022/day14/main.go
+++ go-solutions/2022/day14/main.go
@@ -24,22 +24,6 @@ type Canvas struct {
 	MaxCols int
 }
 
-func (c Canvas) Clone() Canvas {
-	newMap := make(map[Tuple]bool)
-
-	for k, v := range c.Canvas {
-		newMap[k] = v
-	}
-
-	return Canvas{
-		Canvas:  newMap,
-		MinRows: c.MinRows,
-		MaxRows: c.MaxRows,
-		MinCols: c.MinCols,
-		MaxCols: c.MaxCols,
-	}
-}
-
 func parseLines() ([]Chain, error) {
 	items := make([]Chain, 0)
 
@@ -181,51 +165,16 @@ func printGrid(canvas Canvas) {
 
 }
 
-func part1(mapka Canvas) {
+func part1AndPart2(mapka Canvas) {
 	start := Tuple{500, 0}
 	current := start
-	counter := 0
+	part2 := 0
+	part1 := 0
 
 	for {
-		if current[1] > mapka.MaxRows {
-			break
+		if part1 == 0 && current[1] > mapka.MaxRows {
+			part1 = part2
 		}
-		next_row := Tuple{current[0], current[1] + 1}
-
-		is_down_blocked, _ := mapka.Canvas[next_row]
-
-		if !is_down_blocked {
-			current = next_row
-		} else {
-			left := Tuple{next_row[0] - 1, next_row[1]}
-
-			is_left_blocked, _ := mapka.Canvas[left]
-			if !is_left_blocked {
-				current = left
-			} else {
-				right := Tuple{next_row[0] + 1, next_row[1]}
-
-				is_right_blocked, _ := mapka.Canvas[right]
-				if !is_right_blocked {
-					current = right
-				} else {
-					counter = counter + 1
-					mapka.Canvas[current] = true
-					current = start
-				}
-			}
-		}
-	}
-	fmt.Printf("Part 1: %v \n", counter)
-
-}
-
-func part2(mapka Canvas) {
-	start := Tuple{500, 0}
-	current := start
-	counter := 0
-
-	for {
 		next_row := Tuple{current[0], current[1] + 1}
 
 		is_down_blocked, _ := mapka.Canvas[next_row]
@@ -245,7 +194,7 @@ func part2(mapka Canvas) {
 				if !is_right_blocked && (next_row[1] < mapka.MaxRows+2) {
 					current = right
 				} else {
-					counter = counter + 1
+					part2 = part2 + 1
 					mapka.Canvas[current] = true
 
 					if current == start {
@@ -256,8 +205,11 @@ func part2(mapka Canvas) {
 				}
 			}
 		}
+
 	}
-	fmt.Printf("Part 2: %v \n", counter)
+
+	fmt.Printf("Part 1: %v \n", part1)
+	fmt.Printf("Part 2: %v \n", part2)
 
 }
 
@@ -270,23 +222,7 @@ func main() {
 	}
 
 	canvas := createFilledOutCanvas(items)
-	canvas2 := canvas.Clone()
-	printGrid(canvas)
-	part1(canvas)
-	part2(canvas2)
-	printGrid(canvas2)
-
-	// all_in := true
-	// for _, line := range items {
-	// 	for _, item := range line {
-	// 		all_in = all_in && canvas[item]
-	// 	}
-	// }
-
-	//fmt.Printf("All %v items in canvas of len %v: %v\n", len(items), len(mapka), all_in)
-	//fmt.Println("Mapka:")
-
-	// fmt.Println()
-	// fmt.Printf("%v", items[:1])
-
+	//printGrid(canvas)
+	part1AndPart2(canvas)
+	//printGrid(canvas)
 }
